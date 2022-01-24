542539db5635c04518d3e9e4cd06521a612aa1be
tombewley <tom.bewley@bristol.ac.uk>
2022-01-24 17:25:04 +0000

    pbrl.save() makes massive file; need to investigate this

--- rlutils/observers/pbrl.py
+++ rlutils/observers/pbrl.py
@@ -308,6 +308,40 @@ class PbrlObserver:
         print(hr.rules(self.tree, pred_dims="reward"))
                 
     def save(self):
+
+        import sys
+        def get_size(obj, seen=None):
+            """Recursively finds size of objects"""
+            size = sys.getsizeof(obj)
+            if seen is None:
+                seen = set()
+            obj_id = id(obj)
+            if obj_id in seen:
+                return 0
+            # Important mark as seen *before* entering recursion to gracefully handle
+            # self-referential objects
+            seen.add(obj_id)
+            if isinstance(obj, dict):
+                size += sum([get_size(v, seen) for v in obj.values()])
+                size += sum([get_size(k, seen) for k in obj.keys()])
+            elif hasattr(obj, '__dict__'):
+                size += get_size(obj.__dict__, seen)
+            elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
+                size += sum([get_size(i, seen) for i in obj])
+            return size
+        print("=================")
+        print(get_size(self))
+        print("=================")
+        s_sum = 0
+        for k,v in self.__dict__.items():
+            s = get_size(v)
+            print(k, s)
+            if k != "interface": s_sum += s
+        print("=================")
+        print(s_sum)
+        print("=================")
+        raise Exception()
+
         path = f"run_logs/{self.run_names[-1]}"
         if not os.path.exists(path): os.makedirs(path)
         f = self.features; self.features = None # NOTE: Lambda functions can't be Pickled
