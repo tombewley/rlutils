a3dd201595f37fc1453fcfc2e37ab295ae5eae50
tombewley <tom.bewley@bristol.ac.uk>
2022-02-17 11:15:41 +0000

    Implement SimTeacher irrationalities

--- rlutils/observers/pbrl.py
+++ rlutils/observers/pbrl.py
@@ -21,7 +21,7 @@ class PbrlObserver:
         elif type(features) == list: self.feature_names, self.features = features, None
         self.run_names = run_names if run_names is not None else [] # Order crucial to match with episodes.
         self.load_episodes(episodes if episodes is not None else [])
-        self.interface = self.P["interface"][0](self, *self.P["interface"][1:]) if "interface" in self.P else None
+        self.interface = self.P["interface"]["kind"](self, **self.P["interface"]) if "interface" in self.P else None
         self._do_observe = "observe_freq" in self.P and self.P["observe_freq"] > 0
         self._do_online = "feedback_budget" in self.P and self.P["feedback_budget"] > 0
         if self._do_online:
@@ -202,6 +202,7 @@ class PbrlObserver:
                 else: assert k_max == 1; i, j = ij # Force specified i, j.
                 y_ij = self.interface(i, j)
                 if y_ij == "esc": print("=== Feedback exited ==="); break
+                elif y_ij == "skip": print(f"({i}, {j}) skipped"); continue
                 assert 0 <= y_ij <= 1
                 self.Pr[i, j] = y_ij
                 self.Pr[j, i] = 1 - y_ij
@@ -627,8 +628,8 @@ class Interface():
 
 class VideoInterface(Interface):
     def __init__(self, pbrl): 
-        import cv2 # Lazy import
         Interface.__init__(self, pbrl)
+        import cv2 # Lazy import
         self.mapping = {81: 1., 83: 0., 32: 0.5, 27: "esc"}
 
     def __enter__(self):
@@ -664,13 +665,52 @@ class VideoInterface(Interface):
         return self.mapping[key]
 
 class OracleInterface(Interface):
-    def __init__(self, pbrl, oracle): 
+    """
+    Oracle class implementing the five modes of irrationality in the SimTeacher algorithm:
+
+    Lee, K., L. Smith, A. Dragan, and P. Abbeel. 
+    "B-Pref: Benchmarking Preference-Based Reinforcement Learning." 
+    Neural Information Processing Systems (NeurIPS) (2021).
+
+    (1) Myopic/recency bias with discount factor gamma
+    (2) Query skipping if max(ret_i, ret_j) is below d_skip
+        - NOTE: This reduces the effective feedback budget
+    (3) Gaussian noise with standard deviation sigma 
+        - Aanalogous to beta in Bradley-Terry model
+    (4) Random flipping of P_i with probability epsilon
+    (5) Equal preference expression if abs(P_i - 0.5) is below p_equal
+
+    NOTE: Order of implementation here: (1),(2),(3),(4),(5)
+    is different to the original paper: (2),(5),(1),(3),(4).
+
+    Additional features:
+    (6) Return P_i directly rather than a sample from it - likely to improve performance as gives more information
+    TODO:
+    (7) Left-right bias *NEED TO RANDOMISE ORDER OF i,j AFTER SAMPLING FOR THIS TO WORK*
+    """
+    def __init__(self, pbrl, kind=None, oracle=None, gamma=1, sigma=0, d_skip=-np.inf, p_equal=0, epsilon=0, return_P_i=False): 
         Interface.__init__(self, pbrl)
         self.oracle = oracle
+        self.gamma, self.sigma, self.d_skip, self.p_equal, self.epsilon, self.return_P_i = gamma, sigma, d_skip, p_equal, epsilon, return_P_i
 
     def __call__(self, i, j): 
-        diff = (self.oracle[i] - self.oracle[j] if type(self.oracle) == list # Lookup 
-                else sum(self.oracle(self.pbrl.episodes[i])) - sum(self.oracle(self.pbrl.episodes[j]))) # Function
-        if diff > 0: return 1. # Positive diff means i preferred.
-        if diff < 1: return 0.
-        return 0.5
+        if type(self.oracle) == list:
+            raise NotImplementedError("List-based oracle is deprecated")
+            diff = self.oracle[i] - self.oracle[j]
+        else:
+            ret_i = self.myopic_sum(self.oracle(self.pbrl.episodes[i]))
+            ret_j = self.myopic_sum(self.oracle(self.pbrl.episodes[j]))
+            if max(ret_i, ret_j) < self.d_skip: return "skip"
+            if ret_i == ret_j: P_i = 0.5
+            else:
+                with np.errstate(divide="ignore"): # Div/0 is okay here.
+                    P_i = norm.cdf((ret_i - ret_j) / self.sigma)
+            if np.random.rand() <= self.epsilon: P_i = 1 - P_i
+        if self.return_P_i: return P_i
+        elif abs(P_i - 0.5) <= self.p_equal: return 0.5
+        elif np.random.rand() < P_i: return 1. 
+        else: return 0. 
+
+    def myopic_sum(self, rewards):
+        if self.gamma == 1: return sum(rewards)
+        return sum([r*(self.gamma**t) for t,r in enumerate(reversed(rewards))])
