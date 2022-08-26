ac37ff880d9b23f96242608206f7d52cd6e7ca32
tombewley <tom.bewley@bristol.ac.uk>
2022-08-26 17:44:41 +0100

    Improve functionality of experiments.wandb.get

--- experiments/wandb.py
+++ experiments/wandb.py
@@ -1,24 +1,27 @@
 import wandb
 from tqdm import tqdm
-from numpy import array, transpose
+from numpy import array, pad, transpose
 from pandas import DataFrame
 
 
-def get(project_name, metrics, filters=None, tag=None):
+def get(project_name, metrics, filters, tag=None):
     data = [{"run_names": [], "metrics": []} for _ in filters]
     # TODO: Can apply filters directly to api.runs
     for run in tqdm(wandb.Api().runs(project_name)):
         if tag is None or tag in run.tags:
-            config = run.config; config.update({"name": run.name})
+            config = run.config
+            config.update({"name": run.name, "group": run.group, "job_type": run.job_type})
             active_filters = set()
             for i, f in enumerate(filters):
                 activate = True
                 for k, v_filter in f.items():
-                    v = config; invalid = False
+                    v = config; present = True
                     for k_sub in k.split("."):
                         try: v = v[k_sub]
-                        except: invalid = True; break
-                    if invalid or v != v_filter: activate = False; break
+                        except: present = False; break
+                    # NOTE: Filter stays active if not present but v_filter == None
+                    if (not(present) and v_filter is not None) or (present and v != v_filter):
+                        activate = False; break
                 if activate: active_filters.add(i)
             if active_filters:
                 run_metrics = []
@@ -30,7 +33,13 @@ def get(project_name, metrics, filters=None, tag=None):
                     data[i]["metrics"].append(run_metrics)    
     dataframes = [{} for _ in filters]
     for i, f in enumerate(filters):
-        for m, data_m in zip(metrics, transpose(array(data[i]["metrics"]))):
+        if len(data[i]["run_names"]) == 0:
+            print(f"Warning: filter {f} returned no runs"); continue
+        # Pad with NaNs to ensure that all the same length
+        max_length = max(len(r) for r in data[i]["metrics"])
+        data_padded = array([pad(r, ((0,max_length-len(r)),(0,0)), constant_values=float("nan"))
+                             for r in data[i]["metrics"]])
+        for m, data_m in zip(metrics, transpose(data_padded)):
             dataframes[i][m] = {
                 "fname": "-".join([f"{k}={v}" for k,v in f.items()]) + "---" + m + ".csv",
                 "df": DataFrame(data_m, columns=data[i]["run_names"])
