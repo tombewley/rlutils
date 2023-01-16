5a251427ab558c707b956fa5bcf8de0c20dda7b2
Tom Bewley <tom.bewley@bristol.ac.uk>
2023-01-16 10:58:49 +0000

    Import get_device to __init__

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 from .common.make_and_load import make, load
 from .common.params import build_params
-from .common.utils import from_numpy
+from .common.utils import from_numpy, get_device
 from .experiments.deploy import train, deploy
